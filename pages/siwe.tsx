4fd7d02019dc119ea836b1f6828cdd06bbbe8390
Brendan <brendan@vozpc.com>
2022-05-31 21:27:33 -0700

    Updated wagmi and siwe to latest.

--- pages/siwe.tsx
+++ pages/siwe.tsx
@@ -1,28 +1,43 @@
-import { getCsrfToken, signIn } from 'next-auth/react'
-import { SiweMessage } from 'siwe'
-import { useAccount, useConnect, useNetwork, useSignMessage } from 'wagmi'
+import { getCsrfToken, signIn } from "next-auth/react"
+import { useEffect } from "react"
+import { SiweMessage } from "siwe"
+import { useAccount, useConnect, useNetwork, useSignMessage } from "wagmi"
+import { InjectedConnector } from "wagmi/connectors/injected"
 import Layout from "../components/layout"
 
-
 function Siwe() {
-  const [{ data: connectData }, connectAsync] = useConnect()
-  const [, signMessage] = useSignMessage()
+  const { connect } = useConnect({ connector: new InjectedConnector() })
+  const { signMessageAsync } = useSignMessage()
+  const { activeChain } = useNetwork()
+  const { data: accountData } = useAccount()
+
+  useEffect(() => {
+    if (!accountData) {
+      connect()
+    }
+  }, [accountData])
 
   const handleLogin = async () => {
     try {
-      const res = await connectAsync(connectData.connectors[0]);
-      const callbackUrl = '/protected';
+      const callbackUrl = "/protected"
       const message = new SiweMessage({
         domain: window.location.host,
-        address: res.data?.account,
-        statement: 'Sign in with Ethereum to the app.',
+        address: accountData?.address,
+        statement: "Sign in with Ethereum to the app.",
         uri: window.location.origin,
-        version: '1',
-        chainId: res.data?.chain?.id,
-        nonce: await getCsrfToken()
-      });
-      const {data: signature, error} = await signMessage({ message: message.prepareMessage() });
-      signIn('credentials', { message: JSON.stringify(message), redirect: false, signature, callbackUrl });
+        version: "1",
+        chainId: activeChain?.id,
+        nonce: await getCsrfToken(),
+      })
+      const signature = await signMessageAsync({
+        message: message.prepareMessage(),
+      })
+      signIn("credentials", {
+        message: JSON.stringify(message),
+        redirect: false,
+        signature,
+        callbackUrl,
+      })
     } catch (error) {
       window.alert(error)
     }
@@ -30,16 +45,24 @@ function Siwe() {
 
   return (
     <Layout>
-    <button
-      onClick={(e) => {
-        e.preventDefault()
-        handleLogin()
-      }}
-    >
-      Sign-in
-    </button>
+      <button
+        onClick={(e) => {
+          e.preventDefault()
+          handleLogin()
+        }}
+      >
+        Sign-in
+      </button>
     </Layout>
   )
+}
+
+export async function getServerSideProps(context: any) {
+  return {
+    props: {
+      csrfToken: await getCsrfToken(context),
+    },
+  }
 }
 
 Siwe.Layout = Layout
